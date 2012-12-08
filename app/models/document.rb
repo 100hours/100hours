75de6f952a79626644d51084b358d37209fa548f
Riccardo Cambiassi <r.cambiassi@gmail.com>
2012-12-08 19:14:58 +0000

    add document default order by created_at

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,11 +1,13 @@
 class Document < ActiveRecord::Base
   attr_accessible :body, :title
 
+  default_scope order("updated_at DESC")
+
   # # extend FriendlyId
   # friendly_id :title, :use => :slugged
 
   def self.active
-    order("updated_at DESC").first
+    self.first
   end
 
 end
