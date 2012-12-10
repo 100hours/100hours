179a3b57f83e17ae71af613d2cc607b912cf77ef
Riccardo Cambiassi <r.cambiassi@gmail.com>
2012-12-10 00:27:46 +0000

    Read notification list from ENV

--- config/application.rb
+++ config/application.rb
@@ -75,6 +75,6 @@ module HundredHours
     config.assets.version = '1.0'
 
     # feedback notification list
-    config.notify_list = ["bru@codewitch.org"]
+    config.notify_list = ENV['NOTIFY_LIST'].split(':')
   end
 end
